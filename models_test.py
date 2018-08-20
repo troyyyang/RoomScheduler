387435195b3effd70fea2cf34a14fd7f263ba783
dell <troyyyang@gmail.com>
2018-08-20 18:19:22 -0400

    formatting

--- models_test.py
+++ models_test.py
@@ -2,8 +2,8 @@ from models import *
 from datetime import datetime
 import unittest
 
-class TestCalendar(unittest.TestCase):
 
+class TestCalendar(unittest.TestCase):
     def test_add_room(self):
         c = Calendar()
         r = Room("Curie", 6)
@@ -21,19 +21,27 @@ class TestCalendar(unittest.TestCase):
         a = Room("Turing", 10)
         c.add_room(r)
         c.add_room(a)
-        m = Meeting(datetime(2018, 8, 18, 9), datetime(2018, 8, 18, 10), 2, "project")
+        m = Meeting(
+            datetime(2018, 8, 18, 9), datetime(2018, 8, 18, 10), 2, "project")
         self.assertTrue(c.add_meeting(m, r))
-        n = Meeting(datetime(2018, 8, 18, 9, 30), datetime(2018, 8, 18, 10, 30), 2, "project2")
+        n = Meeting(
+            datetime(2018, 8, 18, 9, 30), datetime(2018, 8, 18, 10, 30), 2,
+            "project2")
         self.assertFalse(c.add_meeting(n, r))
-        l = Meeting(datetime(2018, 8, 18, 8, 30), datetime(2018, 8, 18, 9, 20), 2, "project3")
+        l = Meeting(
+            datetime(2018, 8, 18, 8, 30), datetime(2018, 8, 18, 9, 20), 2,
+            "project3")
         self.assertFalse(c.add_meeting(l, r))
-        k = Meeting(datetime(2018, 8, 18, 9, 30), datetime(2018, 8, 18, 10, 30), 2, "project4")
+        k = Meeting(
+            datetime(2018, 8, 18, 9, 30), datetime(2018, 8, 18, 10, 30), 2,
+            "project4")
         self.assertFalse(c.add_meeting(l, r))
-        j = Meeting(datetime(2018, 8, 18, 14, 30), datetime(2018, 8, 18, 15, 30), 2, "project5")
+        j = Meeting(
+            datetime(2018, 8, 18, 14, 30), datetime(2018, 8, 18, 15, 30), 2,
+            "project5")
         self.assertTrue(c.add_meeting(j, r))
         self.assertEquals(len(c.get_meetings()[r]), 2)
         self.assertFalse(c.add_meeting(j, r))
-
 
 
 if __name__ == '__main__':
--- view.py
+++ view.py
@@ -1,12 +1,16 @@
 from models import *
 
-class CommandLineView:
 
+class CommandLineView:
     def __init__(self, Calendar):
         self.calendar = Calendar
+
     def display(self):
         schedule = self.calendar.get_meetings()
         for room in schedule:
             print("Room: " + room.get_name())
             for meeting in schedule[room]:
-                print(meeting.get_name() + "\nstart time " + str(meeting.get_start_time()) + "\nend time " + str(meeting.get_end_time()) + "\nAttendees: " + str(meeting.get_num_people()) + "\n")
+                print(meeting.get_name() + "\nstart time " +
+                      str(meeting.get_start_time()) + "\nend time " +
+                      str(meeting.get_end_time()) + "\nAttendees: " +
+                      str(meeting.get_num_people()) + "\n")
